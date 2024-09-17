93c74f0a61614e8af4578a473ecaee836c5e7d51
Adrian Rico <adrianricoitm@gmail.com>
2024-09-17 14:55:22 -0600

    Update index.js
    Added RENDER DB URL...

--- index.js
+++ index.js
@@ -4,15 +4,18 @@ var mongoose    = require('mongoose')
 var app         = require('./app')
 var port        = 8080
 
-//[1] Conexión a la base de datos...
+// DATABASE CONNECTION...
 mongoose.Promise= global.Promise
-mongoose.connect('mongodb://127.0.0.1:27017/db_maylob')
-        .then(()=>{
-            console.log("[⚑][SERVER] - Conexión a BD: ok")
 
-            //Si la conexión a la BD es exitosa, entonces crear el servidor...
-            app.listen(port,()=>{
-                console.log("[⚑][SERVER] - Corriendo en puerto: "+port)
-            })
+// [!] - Uncomment following line to use with release version...!
+mongoose.connect('mongodb+srv://maylobcontrol:HzZf2OWCKGMzpJAz@maylobdb.u424k.mongodb.net/?retryWrites=true&w=majority&appName=maylobDB')
+
+// [!] - Uncomment following line to use ONLY with local DB for tests...!
+//mongoose.connect('mongodb://127.0.0.1:27017/db_maylob')
+    .then(()=>{
+        console.log("[⚑][SERVER] - Conexión a BD: ok")
+        app.listen(port,()=>{
+            console.log("[⚑][SERVER] - Corriendo en puerto: "+port)
         })
-        .catch(err => console.log(err))
+    })
+    .catch(err => console.log(err))
